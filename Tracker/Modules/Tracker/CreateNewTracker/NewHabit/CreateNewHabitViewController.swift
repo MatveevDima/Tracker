e2f4104872ef00ba13e38becc1f63b4888ce6f41
Дмитрий <monk-dee@mail.ru>
2024-07-25 13:38:42 +0300

    Начал верстать экран создания новой привычки

--- Tracker/Modules/Tracker/CreateNewTracker/NewHabit/CreateNewHabitViewController.swift
+++ Tracker/Modules/Tracker/CreateNewTracker/NewHabit/CreateNewHabitViewController.swift
@@ -11,8 +11,115 @@ final class CreateNewHabitViewController : UIViewController {
     
     weak var delegate: CreateNewTrackerProtocol? 
     
+    private lazy var headerLabel: UILabel = {
+        let headerLabel = UILabel()
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.text = "Новая привычка"
+        headerLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        headerLabel.textColor = .black
+        return headerLabel
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Введите название трекера"
+        textField.font = .systemFont(ofSize: 17, weight: .medium)
+        textField.textColor = .black
+        textField.backgroundColor =  UIColor(named: "light_gray_for_background")
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 16
+        
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
+        
+        textField.leftViewMode = .always
+        textField.addTarget(self, action: #selector(didTapCancelButton), for: .allEditingEvents)
+        textField.clearButtonMode = .whileEditing
+        
+        NSLayoutConstraint.activate([
+            textField.heightAnchor.constraint(equalToConstant: 75)
+        ])
+        
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Actions
+    @objc private func didTapCancelButton() {
+    
+    }
+    
+    // MARK: Setup
+    func setupView() {
+        
+        view.backgroundColor = .white
+        
+        setupHeaderLabel()
+        setupScrollView()
+        setupStackView()
+        setupTextField()
+    }
+    
+    func setupHeaderLabel() {
+        
+        view.addSubview(headerLabel)
+        
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 18),
+            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func setupScrollView() {
+        
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 14),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func setupStackView() {
+        
+        scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32),
+        ])
+    }
+    
+    func setupTextField() {
+        
+        stackView.addArrangedSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 24),
+        ])
     }
 }
 
